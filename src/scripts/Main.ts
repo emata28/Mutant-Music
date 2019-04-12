8b229ab416b4b1402aeb81f868a9d9ad501d25f5
Esteban Mata <esteban1113001@gmail.com>
2019-04-11 22:12:46 -0600

    first commit

---
+++ src/scripts/Main.ts
@@ -0,0 +1,6 @@
+
+import * as fs from 'fs';
+// import { complex as fft } from 'fft';
+import * as WavEncoder from 'wav-encoder';
+// import { default as ft } from 'fourier-transform';
+import * as WavDecoder from 'wav-decoder';

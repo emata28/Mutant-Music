dda4ba333936adba660a054ee4b6128aa804322e
Daar375 <Daar375@gmail.com>
2019-04-30 23:20:29 -0600

    Add files via upload

--- src/scripts/Main.ts
+++ src/scripts/Main.ts
@@ -3,128 +3,186 @@ import * as fs from 'fs';
 import * as WavEncoder from 'wav-encoder';
 // import { default as ft } from 'fourier-transform';
 import * as WavDecoder from 'wav-decoder';
-import { getSector } from './library/sector';
-let SectorsS1 : String[] = ["",""];
-let SectorsS2 : String[] = ["",""];
+import {Pattern} from './library/Patterns';
 
+import {getSector} from './library/sector';
+import Patterns from "../../build/library/Patterns";
+
+let SectorsS1: String[] = ["", ""];
+let SectorsS2: String[] = ["", ""];
+let infoCH1: Pattern[] = [];
+let infoCH2: Pattern[] = [];
 let S1: string = process.argv[3];
 let S2: string = process.argv[4];
 let command: string = process.argv[2];
+
+
 const readFile = (filepath: string) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filepath, (err, buffer) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(buffer);
-        });
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, (err, buffer) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(buffer);
     });
+  });
 };
 
 readFile(S2).then((buffer) => {
-    return WavDecoder.decode(buffer);
-}).then(function(audioData) {
+  return WavDecoder.decode(buffer);
+}).then(function (audioData) {
 
+  let index = 0;
+  let count = 0;
+  let channel1 = "";
+  let channel2 = "";
+  for (let i = 0; i < audioData.channelData[0].length; i += 22) {
 
-    let channel1 ="";
-    let channel2 ="";
-    for(var i=0; i<audioData.channelData[0].length; i+=22) {
+    channel1 = getSector(audioData.channelData[0][i], audioData.channelData[0][i + 22]);
+    channel2 = getSector(audioData.channelData[1][i], audioData.channelData[1][i + 22]);
 
-        channel1= getSector(audioData.channelData[0][i],audioData.channelData[0][i+22]);
-        channel2=getSector(audioData.channelData[1][i],audioData.channelData[1][i+22]);
-
-        SectorsS2[0]=SectorsS2[0]+channel1;
-        SectorsS2[1]=SectorsS2[1]+channel2;
-
+    SectorsS2[0] = SectorsS2[0] + channel1;
+    SectorsS2[1] = SectorsS2[1] + channel2;
+    if (SectorsS2[0].length > 8) {
+      preAnal(SectorsS2[0], infoCH1, count);
+      preAnal(SectorsS2[1], infoCH2, count);
 
     }
 
 
+    count++;
+  }
+  infoCH1 = infoCH1.sort((a, b) => (a.points.length < b.points.length) ? 1 : -1).slice(0, 8);
+  count = 0;
+  let sum = 0;
+  while (count != infoCH1.length) {
+    sum = sum + infoCH1[count].points.length;
+    count++;
+  }
 
-}).then(function(Analyze){
+  count = 0;
+  while (count != infoCH1.length) {
+    infoCH1[count].calcPorcentage(sum);
+    count++;
+  }
 
-    let Combinaciones : String[] = [];
-    let concurrenciaValueCH1 : number[] = [];
-    let concurrenciaValueCH2 : number[] = [];
-    let infoCH1=[];
-    let infoCH2 =[];
-    let temp1 ="";
-
-    let index=0;
-    let count=0;
-    while(count+1!=SectorsS2[0].length){
-        temp1=SectorsS2[0][count]+SectorsS2[0][count+1];
-        index = Combinaciones.indexOf(temp1);
-
-        if(index==-1){
-            Combinaciones.push(temp1);
-            concurrenciaValueCH1.push(1);
-        }else{
-            concurrenciaValueCH1[index]+=1;
-
-        }
+  console.log(infoCH1);
 
 
-        count++;
-    }
-    infoCH1.push(Combinaciones);
-    infoCH1.push(concurrenciaValueCH1);
-    //infoCH2.push(Combinaciones);
-    //infoCH2.push(concurrenciaValueCH2);
-
-    count = 0;
-    //while(count !=5){
-    //    concurrenciaValueCH1.
-   // }
-    console.log(infoCH1);
-
-
-    var topValues = infoCH1.sort((a,b)=>b-a)
-    console.log(topValues); // [ 1, 2, 3, 8, 12 ]
-
-    let top3Number;
 })
 
 
-
 readFile(S1).then((buffer) => {
-    return WavDecoder.decode(buffer);
-}).then(function(audioData) {
+  return WavDecoder.decode(buffer);
+}).then(function (audioData) {
 
 
-    let channel1 ="";
-    let channel2 ="";
-    for(var i=0; i<audioData.channelData[0].length; i+=22) {
+  let channel1 = "";
+  let channel2 = "";
+  for (var i = 0; i < audioData.channelData[0].length; i += 22) {
 
-        channel1= getSector(audioData.channelData[0][i],audioData.channelData[0][i+22]);
-        channel2=getSector(audioData.channelData[1][i],audioData.channelData[1][i+22]);
+    channel1 = getSector(audioData.channelData[0][i], audioData.channelData[0][i + 22]);
+    channel2 = getSector(audioData.channelData[1][i], audioData.channelData[1][i + 22]);
 
-        SectorsS1[0]=SectorsS1[0]+channel1;
-        SectorsS1[1]=SectorsS1[1]+channel2;
+    SectorsS1[0] = SectorsS1[0] + channel1;
+    SectorsS1[1] = SectorsS1[1] + channel2;
+  }
+  let porcentage = compareSegement(SectorsS2[0].length / 2, SectorsS1[0].slice(0, SectorsS2[0].length), infoCH1);
+  if (porcentage > 70) {
 
-
-    }
-    console.log("S2");
-    console.log(SectorsS2[0].length);
-    console.log("S1");
-    console.log(SectorsS1[0].length);
-
-
+  }
 });
-function  indexOfMax(arr: number[]) {
-    if (arr.length === 0) {
-        return -1;
-    }
 
-    var max = arr[0];
-    var maxIndex = 0;
 
-    for (var i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            maxIndex = i;
-            max = arr[i];
-        }
-    }
+function preAnal(pCHString: String, infoCH: Pattern[], count: number) {
+  let temp1 = "";
+  let tempPat;
+  for (let e = 1; e < 7; e++) {
+    temp1 += pCHString[pCHString.length - e];
+  }
+  tempPat = infoCH.find(obj => obj._pattern == temp1);
 
-    return maxIndex;
+  if (tempPat == undefined) {
+
+    tempPat = new Pattern(temp1);
+    tempPat.addPoint(count);
+    infoCH.push(tempPat);
+  } else {
+    tempPat.addPoint(count);
+
+  }
 }
+
+
+function compareSegement(pCompar: number, pS1Segment: String, pPorcentages: Pattern[]): number {
+  let count = 0;
+  let rPoint;
+  let tempString: String = "";
+  let pFinded;
+  let index = 0;
+  let pPatterFound: String[] = [];
+  let pCoundFound: number[] = [];
+
+  for (const i in pPorcentages) {
+    pPatterFound.push(pPorcentages[i]._pattern);
+    pCoundFound.push(0);
+  }
+
+
+  while (pCompar != 0) {
+    rPoint = Math.floor(Math.random() * pS1Segment.length);
+    pCompar--;
+    tempString = "";
+    count = 0;
+    while (count != pPorcentages[0]._pattern.length) {
+      tempString = tempString + pS1Segment[rPoint + count];
+
+
+      count++;
+    }
+    index = pPatterFound.indexOf(tempString);
+    if (index != -1) {
+      pCoundFound[index] = pCoundFound[index] + 1;
+
+
+    }
+  }
+
+  count = 0;
+  let sum = 0;
+  while (count != pCoundFound.length) {
+    sum = sum + pCoundFound[count];
+    count++;
+  }
+
+  count = 0;
+  while (count != pCoundFound.length) {
+    pCoundFound[count] = (pCoundFound[count]) * 100 / sum;
+    count++;
+  }
+
+
+  count = 0;
+  let trues = 0;
+  let falses = 0;
+
+  while (count != pPatterFound.length) {
+
+    if (pCoundFound[count] > pPorcentages[count].porcentage - 3 && pCoundFound[count] < pPorcentages[count].porcentage + 3) {
+      trues++;
+    } else {
+      falses++;
+    }
+    count++;
+  }
+  return (trues) * 100 / (trues + falses);
+
+}
+
+
+
+
+
+
+
+
--- src/scripts/library/sector.ts
+++ src/scripts/library/sector.ts
@@ -1,8 +1,7 @@
 export function getSector(pNum1: number,pNum2: number): string {
   if(pNum1<0.0001&&pNum2<0.0001&&pNum1>-0.0001&&pNum2>-0.0001){
     return  "F"
-  }
-  else if(-0.001<pNum1-pNum2&&pNum1-pNum2<=0.001){
+  }else if(-0.001<pNum1-pNum2&&pNum1-pNum2<=0.001){
     return "P";
   }else if(pNum1-pNum2>=0.001){
     return "B";

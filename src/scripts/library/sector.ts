e2df0307e15659e6927b29ed1316986ccd4c281d
Daar375 <Daar375@gmail.com>
2019-04-30 23:21:04 -0600

    Add files via upload

--- src/scripts/library/sector.ts
+++ src/scripts/library/sector.ts
@@ -1,7 +1,9 @@
-export function getSector(pNum: number): string {
-  if(-0.001<pNum&&pNum<=0.001){
+export function getSector(pNum1: number,pNum2: number): string {
+  if(pNum1<0.0001&&pNum2<0.0001&&pNum1>-0.0001&&pNum2>-0.0001){
+    return  "F"
+  }else if(-0.001<pNum1-pNum2&&pNum1-pNum2<=0.001){
     return "P";
-  }else if(pNum>=0.001){
+  }else if(pNum1-pNum2>=0.001){
     return "B";
   }else {
     return "S";
